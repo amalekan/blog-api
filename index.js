59621b413b499f1234ab699c5f6592e8d39e4371
Alexander Malekan <alexmalekan@gmail.com>
2017-07-20 19:10:26 -0400

    turned on the cors with the middlware

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const server = express();
 const morgan = require('morgan');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 
 server.use(morgan('dev'));
+server.use(cors());
 
 server.get('/', (req, res) => {
      res.send('it works');
