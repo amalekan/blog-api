7b141ab2282574a18290c8d51752607dd1e79fdf
Alexander Malekan <alexmalekan@gmail.com>
2017-07-31 17:02:17 -0400

    Added setPassword and isValidPassword to the user model. Ade user model. Additionally, changed the user.router to setPassword when the user id when the user is created. Validated that the request has a password field.

--- routers/user.router.js
+++ routers/user.router.js
@@ -24,7 +24,13 @@ router.get('/users/:userId',(req, res) => {
 });
 
 router.post('/users', (req, res) => {
+    if(!req.body.password){
+      return res.status(400).json({
+        msg: 'Bad request'
+      });
+    }
     const newUser = new User(req.body);
+    newUser.setPassword(req.body.password);
     newUser.save(function (err, user) {
       if(err) return res.status(500).json({err: err});
       return res.status(201).json({
