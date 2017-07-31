c401826bebbaedfe1121de694e1b42442f4e05fa
Alexander Malekan <alexmalekan@gmail.com>
2017-07-31 17:11:02 -0400

    Created the post model using a schema that requires that a post belongs to a user.

---
+++ models/post.model.js
@@ -0,0 +1,29 @@
+/*jshint esversion: 6 */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = Schema({
+  title: {
+    required: true,
+    type: String
+  },
+
+  body: {
+    required: true,
+    type: String
+  },
+
+  created: {
+    type: Date,
+    default: Date.now
+  },
+
+  user: {
+    required: true,
+    type: Schema.ObjectId
+  }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
