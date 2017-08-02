527fbefa469dc76697fffdb345c1fa8a7e9b3afb
Alexander Malekan <alexmalekan@gmail.com>
2017-08-02 18:31:14 -0400

    prepped for herkou deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const server = express();
-const {mongoURI} = require('./credentials');
+const mongoURI = process.env.MONGOURI || require('./credentials').mongoURI;
 const port = process.env.PORT || 8080;
 
 mongoose.connect(mongoURI, {
